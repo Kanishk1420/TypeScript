147f80845a44f3b540fba86e99b72c10d47a4554
Kanishk <Kanishkgupta2003@outlook.com>
2026-06-24 16:18:17 +0530

    Type Guard

--- src/Topic 5- MakingCustomTypes.ts
+++ src/Topic 5- MakingCustomTypes.ts
@@ -25,3 +25,45 @@ console.log(
   printBioData({ name: "Kanishk", age: 25, rollno: 101, isActive: true }),
   printBioData("hello"), // This will return "Invalid biodata object: hello" because the input is not of type biodata
 );
+
+type Ball = {
+  type: "Ball"
+  color: string;
+  amount: number;
+}
+type Bat = {
+  type: "Bat"
+  hardness: string;
+  amount: number;
+}
+type Gloves = {
+  type: "Gloves"
+  color: string;
+  amount: number;
+}
+type SportsEquipment = Ball | Bat | Gloves;
+
+const getEquipment = (item: SportsEquipment) => {
+  switch (item.type) {
+    case "Ball":
+      return `This is a ${item.color} ball with amount ${item.amount}`;
+    case "Bat":
+      return `This is a ${item.hardness} bat with amount ${item.amount}`;
+    case "Gloves":
+      return `These are ${item.color} gloves with amount ${item.amount}`;
+    default:
+      return "Invalid sports equipment";
+  }
+}
+const putequipment = (item: SportsEquipment) => {
+  if("color" in item){
+    return `This is a ${item.color} ${item.type}`;
+  }
+  else{
+    return `This is a ${item.hardness} ${item.type}`;
+  }
+}
+
+function isStringArray(arr: unknown[]): arr is string[] {
+  return arr.every((item) => typeof item === "string");
+}
